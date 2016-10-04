8cc0c020de1612e32273a7f6d310161794de88ea
Попов Федор Павлович <eynsher_kiel@outlook.com>
2016-10-05 01:22:16 +0300

    Project setup

---
+++ Twill.UI.Core/Tools/Notify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twill.UI.Core.Tools
+{
+    public class Notify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+    }
+}
---
+++ Twill.UI.Core/Tools/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Twill.UI.Core.Tools
+{
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action handler)
+        {
+            _handler = handler;
+        }
+
+        private readonly Action _handler;
+   
+
+        public bool CanExecute(object parameter) => true;
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void Execute(object parameter) => _handler();
+    }
+}
